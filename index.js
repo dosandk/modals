2a4cf44d28085820ede1b73704281b1187d7d07e
Vladimir Shevchuk <dosandkv@gmail.com>
2016-03-19 21:02:53 +0200

    added base structure;

---
+++ index.js
@@ -0,0 +1,45 @@
+window.modals = (function() {
+    var defaultConfig = {
+        title: 'title',
+        content: 'content',
+        closeBtn: 'close'
+    };
+
+    function mergeConfig(customConf) {
+        var mergedConf = {};
+
+        for (var i in defaultConfig) {
+            if (customConf[i]) {
+                mergedConf[i] = customConf[i];
+            }
+            else {
+                mergedConf[i] = defaultConfig[i];
+            }
+        }
+
+        return mergedConf;
+    }
+
+    function createModal() {
+        console.error('someFunction1');
+    }
+
+    return {
+        someMethod1: function() {
+            console.error(1);
+        },
+        someMethod2: function() {
+            console.error(2);
+        },
+        someMethod3: function() {
+            console.error(3);
+        },
+        getConfig: function() {
+            return defaultConfig;
+        },
+        initModal: function(config) {
+
+            return mergeConfig(config);
+        }
+    };
+}());
