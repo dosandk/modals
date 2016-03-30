91111f90fa15bd6e4d1c129526ef54b2bef7d95a
Vladimir Shevchuk <dosandkv@gmail.com>
2016-03-30 20:53:22 +0300

    add destroy method;

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 window.modals = (function() {
     var defaultConfig = {
+        selector: '.modal',
         title: 'title',
         content: 'content',
         closeBtn: 'close'
@@ -20,8 +21,13 @@ window.modals = (function() {
         return mergedConf;
     }
 
-    function createModal() {
+    function CreateModal() {
         console.error('someFunction1');
+    }
+
+    function handler(e) {
+        console.log(e.target.innerHTML);
+        console.log(this.modal);
     }
 
     return {
@@ -38,8 +44,28 @@ window.modals = (function() {
             return defaultConfig;
         },
         initModal: function(config) {
+            var currentConfig = mergeConfig(config);
+            var selector = currentConfig.selector;
+            var elements = document.querySelectorAll(selector);
+            var elementsArr = Array.prototype.slice.call(elements);
 
-            return mergeConfig(config);
+            elementsArr.forEach(function(element) {
+                element.modal = {};
+                element.modal.config = currentConfig;
+                element.addEventListener('click', handler);
+            });
+
+            return elementsArr;
+        },
+        destroyModal: function(config) {
+            var elements = document.querySelectorAll(config.selector);
+            var elementsArr = Array.prototype.slice.call(elements);
+
+            elementsArr.forEach(function(element) {
+                element.removeEventListener('click', handler);
+            });
+
+            return elementsArr;
         }
     };
 }());
